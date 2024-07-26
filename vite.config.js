8ee7b765814932db4168a36152b1bdb69b2941a2
devkyoung2 <devkyoung2@gmail.com>
2024-07-26 17:15:08 +0900

    [chore] : 절대경로 지정

---
+++ vite.config.js
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+const __dirname = path.resolve();
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@assets',
+        replacement: path.resolve(__dirname, 'src/assets'),
+      },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '@hooks',
+        replacement: path.resolve(__dirname, 'src/hooks'),
+      },
+      {
+        find: '@layouts',
+        replacement: path.resolve(__dirname, 'src/layouts'),
+      },
+      {
+        find: '@pages',
+        replacement: path.resolve(__dirname, 'src/pages'),
+      },
+      {
+        find: '@services',
+        replacement: path.resolve(__dirname, 'src/services'),
+      },
+      {
+        find: '@types',
+        replacement: path.resolve(__dirname, 'src/types'),
+      },
+      {
+        find: '@utils',
+        replacement: path.resolve(__dirname, 'src/utils'),
+      },
+    ],
+  },
+});
