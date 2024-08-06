d13f3c2cd6ddf38d65798f9d1e9886b026a3e716
devkyoung2 <devkyoung2@gmail.com>
2024-08-06 16:54:29 +0900

    [feat] : 서치바 컴포넌트 기능 구현(#19)
    - submit 시 API 요청
    - input value 상태관리
    - delete button 동작

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { BiX } from 'react-icons/bi';
 import { FcSearch } from 'react-icons/fc';
 
@@ -6,16 +7,34 @@ type SearchBarProps = {
 };
 
 const SearchBar = ({ placeholder }: SearchBarProps) => {
+  const [value, setValue] = useState<string>('');
+
+  const handleClear = () => setValue('');
+
+  const handleSubmit = () => {
+    console.log('API 요청');
+  };
   return (
-    <div className="mx-5 my-3 flex items-center rounded-xl border border-solid px-3 py-1">
+    <form
+      onSubmit={handleSubmit}
+      className="mx-5 my-3 flex items-center rounded-xl border border-solid px-3 py-1"
+    >
       <span className="mr-1">
         <FcSearch style={{ width: '28px', height: '28px' }} />
       </span>
-      <input className="h-[36px] w-full focus:outline-none" placeholder={placeholder} />
-      <span className="ml-1">
-        <BiX style={{ width: '24px', height: '24px' }} />
-      </span>
-    </div>
+      <input
+        className="h-[36px] w-full focus:outline-none"
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      {value && (
+        <button type="button" onClick={handleClear} aria-label="clear button" className="ml-1">
+          <BiX style={{ width: '24px', height: '24px' }} />
+        </button>
+      )}
+      <button type="submit" className="hidden" aria-label="submit button" />
+    </form>
   );
 };
 
