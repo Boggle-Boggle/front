10091b124791e181034740d0be8c1dfda63bea43
devkyoung2 <devkyoung2@gmail.com>
2024-08-06 15:38:52 +0900

    [feat] : 검색결과 페이지 내부 레이아웃 구현 및 왼쪽 상단 아이콘 클릭시 라우팅(#18)

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,10 +1,16 @@
 import { BiScan } from 'react-icons/bi';
 import { IoArrowBackOutline } from 'react-icons/io5';
+import { TbCameraSearch } from 'react-icons/tb';
+import { useNavigate } from 'react-router-dom';
 
 import Header from 'components/ui/Header';
 
 const Search = () => {
-  const handleBack = () => {};
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
   const handleReadBarcode = () => {};
   return (
     <>
@@ -12,14 +18,22 @@ const Search = () => {
         title={{ text: '도서 검색' }}
         leftBtn={{
           icon: <IoArrowBackOutline style={{ width: '24px', height: '24px' }} />,
-          handleLeftBtnClick: handleBack,
+          handleLeftBtnClick: handleGoBack,
         }}
         rightBtn={{
           icon: <BiScan style={{ width: '24px', height: '24px' }} />,
           handleRightBtnClick: handleReadBarcode,
         }}
       />
-      검색결과페이지
+      <div>서치바</div>
+      <div>최근검색어</div>
+      <div className="flex flex-col items-center justify-center pt-28">
+        <TbCameraSearch style={{ width: '137px', height: '137px', opacity: '30%' }} />
+        <p className="pt-4 text-center">
+          오른쪽 상단의 아이콘을 클릭하면 <br />
+          바코드 검색이 가능합니다.
+        </p>
+      </div>
     </>
   );
 };
