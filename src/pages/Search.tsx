f22f05e51bf77673c77c2a0e4539e92cc06e9468
devkyoung2 <devkyoung2@gmail.com>
2024-08-21 17:02:50 +0900

    [feat] : 검색 결과 화면 구현(#27)

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { BiScan } from 'react-icons/bi';
 import { IoArrowBackOutline } from 'react-icons/io5';
 import { TbCameraSearch } from 'react-icons/tb';
@@ -8,14 +9,22 @@ import Header from 'components/ui/Header';
 import SearchBar from 'components/ui/SearchBar';
 
 const Search = () => {
+  const [value, setValue] = useState<string>('');
+  const [bookData, setBookData] = useState<null | number[]>([1, 2, 3]);
   const navigate = useNavigate();
 
   const handleGoBack = () => {
     navigate(-1);
   };
+
   const handleReadBarcode = () => {};
+
+  const handleSubmit = () => {
+    console.log('API 요청');
+  };
+
   return (
-    <>
+    <div className="h-screen bg-[#DCD7D6]">
       <Header
         title={{ text: '도서 검색' }}
         leftBtn={{
@@ -27,16 +36,29 @@ const Search = () => {
           handleRightBtnClick: handleReadBarcode,
         }}
       />
-      <SearchBar placeholder="읽고 싶은 책을 검색해 보세요!" />
+      <SearchBar
+        placeholder="읽고 싶은 책을 검색해 보세요!"
+        value={value}
+        setValue={setValue}
+        handleSubmit={handleSubmit}
+      />
       <SearchHistory />
-      <div className="flex flex-col items-center justify-center pt-28">
-        <TbCameraSearch style={{ width: '137px', height: '137px', opacity: '30%' }} />
-        <p className="pt-4 text-center">
-          오른쪽 상단의 아이콘을 클릭하면 <br />
-          바코드 검색이 가능합니다.
-        </p>
-      </div>
-    </>
+      {bookData !== null ? (
+        <ul className="absolute bottom-0 mb-[84px] h-[620px] w-full rounded-tl-3xl bg-white p-6">
+          {bookData.map((book) => (
+            <li>{book}</li>
+          ))}
+        </ul>
+      ) : (
+        <div className="flex flex-col items-center justify-center pt-28">
+          <TbCameraSearch style={{ width: '137px', height: '137px', opacity: '30%' }} />
+          <p className="pt-4 text-center">
+            오른쪽 상단의 아이콘을 클릭하면 <br />
+            바코드 검색이 가능합니다.
+          </p>
+        </div>
+      )}
+    </div>
   );
 };
 
