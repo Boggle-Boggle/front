14df41692cd4377e24d2af111a9805a51cb39008
devkyoung2 <devkyoung2@gmail.com>
2024-11-14 20:25:32 +0900

    [docs] : 불필요한 주석 삭제

--- src/pages/BookDetail/ReadingRecordForm/index.tsx
+++ src/pages/BookDetail/ReadingRecordForm/index.tsx
@@ -9,15 +9,16 @@ import Visiable from './Visible';
 import Complete from './Complete';
 
 type StepType = '상태' | '별점' | '날짜' | '서재' | '숨기기' | '완료';
+type ReadingRecordFormProps = {
+  onClose: () => void;
+};
 
-const ReadingRecordForm = () => {
-  const [nickName, setNickName] = useState<string>('');
+const ReadingRecordForm = ({ onClose }: ReadingRecordFormProps) => {
   const [step, setStep] = useState<StepType>('상태');
-  const onClose = () => {};
 
   return (
     <>
-      <div className="absolute z-20 h-full w-full bg-black opacity-40" />
+      <div className="absolute z-20 h-full w-full bg-black opacity-40" onClick={onClose} />
       <section className="fixed bottom-0 z-30 h-auto w-full rounded-t-2xl bg-main p-10">
         {step === '상태' && <Status onNext={() => setStep('별점')} />}
         {step === '별점' && <Rating onPrev={() => setStep('상태')} onNext={() => setStep('날짜')} />}
@@ -25,7 +26,6 @@ const ReadingRecordForm = () => {
         {step === '서재' && <Library onPrev={() => setStep('날짜')} onNext={() => setStep('숨기기')} />}
         {step === '숨기기' && <Visiable onPrev={() => setStep('서재')} onNext={() => setStep('완료')} />}
         {step === '완료' && <Complete />}
-
         <button type="button" onClick={onClose} className="absolute right-3 top-3">
           <FaXmark style={{ width: '20px', height: '20px' }} />
         </button>
@@ -35,4 +35,3 @@ const ReadingRecordForm = () => {
 };
 
 export default ReadingRecordForm;
-// <NickNameInput nickName={nickName} setNickName={setNickName} onNext={() => setStep('약관동의')} />;
