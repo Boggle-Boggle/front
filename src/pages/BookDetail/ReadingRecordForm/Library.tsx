ec94a488ef354613d5de2996787e5cd7a4d83ccf
devkyoung2 <devkyoung2@gmail.com>
2024-11-22 21:12:20 +0900

    [feat] : 서재 조건부 렌더링 위치 변경

--- src/pages/BookDetail/ReadingRecordForm/Library.tsx
+++ src/pages/BookDetail/ReadingRecordForm/Library.tsx
@@ -26,12 +26,12 @@ const Library = ({ selected, setSelected, onPrev, onNext }: LibraryProps) => {
   };
 
   return (
-    data && (
-      <>
-        <Title message="책을 서재에 분류해 보세요!" />
-        <SubTitle message="필요한 서재가 없다면 아래에서 추가해보세요" />
-        <ul className="max-h-96 overflow-y-scroll">
-          {data.map(({ libraryId, libraryName }) => (
+    <>
+      <Title message="책을 서재에 분류해 보세요!" />
+      <SubTitle message="필요한 서재가 없다면 아래에서 추가해보세요" />
+      <ul className="max-h-96 overflow-y-scroll">
+        {data &&
+          data.map(({ libraryId, libraryName }) => (
             <li
               className={`mb-4 flex justify-between rounded-[10px] bg-white p-4 text-[15px] shadow-md ${selected.includes(libraryId) && `border-2 border-accent`}`}
               onClick={() => handleSelect(libraryId)}
@@ -41,15 +41,14 @@ const Library = ({ selected, setSelected, onPrev, onNext }: LibraryProps) => {
             </li>
           ))}
 
-          <li className="flex items-center justify-center rounded-[10px] bg-white p-4 text-[15px] shadow-md">
-            <FiPlus style={{ width: '16px', height: '16px', marginRight: '4px' }} />
-            서재 추가하기
-          </li>
-        </ul>
+        <li className="flex items-center justify-center rounded-[10px] bg-white p-4 text-[15px] shadow-md">
+          <FiPlus style={{ width: '16px', height: '16px', marginRight: '4px' }} />
+          서재 추가하기
+        </li>
+      </ul>
 
-        <ButtonSet onPrev={onPrev} onNext={onNext} />
-      </>
-    )
+      <ButtonSet onPrev={onPrev} onNext={onNext} />
+    </>
   );
 };
 
