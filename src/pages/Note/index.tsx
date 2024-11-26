d65e39ae9dc3cc07d76a5dfc534c741ceb6d70c4
devkyoung2 <devkyoung2@gmail.com>
2024-11-27 02:39:23 +0900

    [feat] : 독서노트 작성 페이지 레이아웃 구현

---
+++ src/pages/Note/index.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PiBooksDuotone } from 'react-icons/pi';
+import { IoArrowBackOutline } from 'react-icons/io5';
+import Header from 'components/ui/Header';
+
+import bookmark from 'assets/bookmarkBig.png';
+
+const Note = () => {
+  const [title, setTitle] = useState<string>();
+  const [content, setContent] = useState<string>();
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+  return (
+    <div className="relative h-screen bg-gray">
+      <Header
+        title={{ text: '독서 노트' }}
+        leftBtn={{
+          icon: <IoArrowBackOutline style={{ width: '24px', height: '24px' }} />,
+          handleLeftBtnClick: handleGoBack,
+        }}
+        rightBtn={{
+          icon: <span>저장</span>,
+          handleRightBtnClick: () => console.log('독서노트저장'),
+        }}
+      />
+      <section className="height-without-footer pb-header flex flex-col overflow-hidden rounded-tl-3xl bg-white">
+        <img src={bookmark} className="h-13 header absolute left-10 top-[63px] block w-12" />
+        <input
+          className={`w-full p-4 text-center focus:outline-none`}
+          value={title}
+          placeholder="제목을 작성해주시핑"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          className={`w-full flex-grow resize-none overflow-auto p-3 focus:outline-none`}
+          value={content}
+          placeholder="내용을 작성해주시핑"
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <div className="flex h-10 items-center justify-between bg-main">
+          <section>
+            <button className="px-3 py-2" onClick={() => {}} type="button">
+              <PiBooksDuotone style={{ width: '24px', height: '24px', color: '#9B9999' }} />
+            </button>
+            <button className="px-3 py-2" onClick={() => {}} type="button">
+              <PiBooksDuotone style={{ width: '24px', height: '24px', color: '#9B9999' }} />
+            </button>
+            <button className="px-3 py-2" onClick={() => {}} type="button">
+              <PiBooksDuotone style={{ width: '24px', height: '24px', color: '#9B9999' }} />
+            </button>
+            <button className="px-3 py-2" onClick={() => {}} type="button">
+              <PiBooksDuotone style={{ width: '24px', height: '24px', color: '#9B9999' }} />
+            </button>
+          </section>
+
+          <span className="pr-3 text-sm">0자/256자</span>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Note;
