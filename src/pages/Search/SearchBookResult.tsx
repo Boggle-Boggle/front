824e4cc714d0b73db9f4414cdc7b8c32b18b585b
devkyoung2 <devkyoung2@gmail.com>
2024-11-26 20:01:51 +0900

    [fix] : 도서검색 결과에서 책 이미지 높이와 책제목 높이가 안맞는 문제 수정

--- src/pages/Search/SearchBookResult.tsx
+++ src/pages/Search/SearchBookResult.tsx
@@ -10,14 +10,10 @@ const SearchBookResult = ({ book }: SearchBookResultProps) => {
 
   return (
     <div className="flex h-[126px] items-center p-2">
-      <img
-        src={book.cover}
-        className="mr-3 h-[100px] w-[70px] drop-shadow-md"
-        alt={`${book.title}책 커버`}
-      />
+      <img src={book.cover} className="mr-3 h-full w-[70px] drop-shadow-md" alt={`${book.title}책 커버`} />
       <div className="flex h-full flex-grow flex-col justify-between">
         <span className="line-clamp-1 text-sm font-semibold">{book.title}</span>
-        <div className="text-sub text-[11px]">
+        <div className="text-[11px] text-sub">
           <div className="line-clamp-1">{`저자 ${book.author}`}</div>
           <div className="line-clamp-1">{`출판 ${book.publisher}`}</div>
           <div>{`${yy}년 ${mm}월 ${dd}일`}</div>
