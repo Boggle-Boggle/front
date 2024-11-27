d9819121a78b7f2ee6e12dd9a9175380ef5e5393
devkyoung2 <devkyoung2@gmail.com>
2024-11-27 15:44:52 +0900

    [feat] : 노트 작성 페이지와 노트 조회 페이지를 분리

--- src/pages/Record/index.tsx
+++ src/pages/Record/index.tsx
@@ -1,9 +1,17 @@
+import { useParams, useNavigate } from 'react-router-dom';
+
 const Record = () => {
+  const { recordId } = useParams();
+  const navigate = useNavigate();
+  const handleGoToNote = () => {
+    navigate(`/note/write`, { state: { recordId } });
+  };
+
   return (
     <>
       <div className="h-80 w-full bg-slate-300">노트 조회</div>
       <div>
-        <button onClick={() => {}}>노트 작성하기</button>
+        <button onClick={handleGoToNote}>노트 작성하기</button>
       </div>
       <div>
         <button>노트 수정하기</button>
