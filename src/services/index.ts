b9884c9f077f508d18f9acd64c70982f0ccd6802
devkyoung2 <devkyoung2@gmail.com>
2024-11-28 14:59:43 +0900

    [feat] : 엑세스 토큰 만료 시 리프레시 토큰을 통해 엑세스 토큰을 재발급 받는 로직 구현

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,20 +1,19 @@
 import axios from 'axios';
-
-import { getSessionItem } from 'utils/sessions';
+import { getSessionItem, removeSessionItem, setSessionItem } from 'utils/sessions';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_SERVER_BASE_URL,
   timeout: 1000,
+  withCredentials: true,
 });
 
 api.interceptors.request.use(
   (config) => {
-    const newConfig = config;
+    const newConfig = { ...config };
     const accessToken = getSessionItem('accessToken');
 
     if (accessToken) {
       newConfig.headers.Authorization = `Bearer ${accessToken}`;
-      newConfig.withCredentials = true;
     }
 
     return newConfig;
@@ -24,4 +23,33 @@ api.interceptors.request.use(
   },
 );
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+
+  async (error) => {
+    const { response, config } = error;
+
+    if (response.status === 401) {
+      try {
+        const refreshResponse = await api.get('/auth/refresh');
+        const newAccessToken = refreshResponse.data.data;
+        const newConfig = { ...config };
+
+        removeSessionItem('accessToken');
+        setSessionItem('accessToken', newAccessToken);
+
+        newConfig.headers.Authorization = `Bearer ${newAccessToken}`;
+        return api(newConfig);
+      } catch (err) {
+        // TODO : 로그인 재시도 안내 구현
+        removeSessionItem('accessToken');
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 export default api;
