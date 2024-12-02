5ae1da80d913f27130a9dc14d6529a4cc6565db5
devkyoung2 <devkyoung2@gmail.com>
2024-12-02 16:10:47 +0900

    [feat] : 책의 두께를 100페이지 단위로 조정 및 offset 값 변경

--- src/pages/Home/BookCase.tsx
+++ src/pages/Home/BookCase.tsx
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { useMemo, Suspense, useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { Canvas } from '@react-three/fiber';
 import { Html, OrbitControls, useGLTF } from '@react-three/drei';
@@ -9,27 +9,14 @@ import { getBookCase } from 'services/record.ts';
 import Book from './Book.tsx';
 
 const getBookProperties = (page: number) => {
-  if (page <= 50) return { width: 0.55, offset: 0.045 };
-  if (page <= 100) return { width: 0.5, offset: 0.05 };
-  if (page <= 150) return { width: 0.55, offset: 0.055 };
-  if (page <= 200) return { width: 0.6, offset: 0.06 };
-  if (page <= 250) return { width: 0.65, offset: 0.065 };
-  if (page <= 300) return { width: 0.7, offset: 0.07 };
-  if (page <= 350) return { width: 0.75, offset: 0.075 };
-  if (page <= 400) return { width: 0.8, offset: 0.08 };
-  if (page <= 450) return { width: 0.85, offset: 0.085 };
-  if (page <= 500) return { width: 0.9, offset: 0.09 };
-  if (page <= 550) return { width: 0.95, offset: 0.095 };
-  if (page <= 600) return { width: 1.0, offset: 0.1 };
-  if (page <= 650) return { width: 1.05, offset: 0.105 };
-  if (page <= 700) return { width: 1.1, offset: 0.11 };
-  if (page <= 750) return { width: 1.15, offset: 0.115 };
-  if (page <= 800) return { width: 1.2, offset: 0.12 };
-  if (page <= 850) return { width: 1.25, offset: 0.125 };
-  if (page <= 900) return { width: 1.3, offset: 0.13 };
-  if (page <= 950) return { width: 1.35, offset: 0.135 };
-
-  return { width: 1.4, offset: 0.14 };
+  if (page <= 100) return { width: 0.7, offset: 0.07 };
+  if (page <= 200) return { width: 0.9, offset: 0.09 };
+  if (page <= 300) return { width: 1.1, offset: 0.11 };
+  if (page <= 400) return { width: 1.3, offset: 0.13 };
+  if (page <= 500) return { width: 1.5, offset: 0.15 };
+  if (page <= 600) return { width: 1.7, offset: 0.17 };
+  if (page <= 700) return { width: 1.9, offset: 0.19 };
+  return { width: 2.1, offset: 0.21 };
 };
 
 const BookCase = () => {
