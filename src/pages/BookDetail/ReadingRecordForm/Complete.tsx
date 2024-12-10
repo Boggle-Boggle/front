b3fcaf7ea480e9e004c13c800af211f9143a34ba
devkyoung2 <devkyoung2@gmail.com>
2024-12-11 02:15:28 +0900

    [lint] : 장식용 이미지에 빈 alt 태그 달기

--- src/pages/BookDetail/ReadingRecordForm/Complete.tsx
+++ src/pages/BookDetail/ReadingRecordForm/Complete.tsx
@@ -39,7 +39,7 @@ const Complete = ({ record }: CompleteType) => {
     <>
       <Title message="책이 등록되었어요!" />
       <SubTitle message="기록을 통해 독서노트도 작성해보세요" />
-      <img src={visible} className="mx-auto"></img>
+      <img src={visible} className="mx-auto" alt="" />
       <Button handleClick={handleGoRecord}>독서 노트 작성하기</Button>
       <Button handleClick={() => navigate('/search')} className="mt-4 w-full bg-main text-black shadow-sm">
         이어서 도서 등록하기
