4f0ed7e0807b3d7f70b68774ebb72e1aec3a0227
devkyoung2 <devkyoung2@gmail.com>
2024-12-14 20:19:04 +0900

    [feat] : 줄거리 줄수제한 및 더보기 visible 여부 적용

--- src/pages/BookDetail/index.tsx
+++ src/pages/BookDetail/index.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { IoArrowBackOutline } from 'react-icons/io5';
 import { useNavigate, useParams } from 'react-router-dom';
 
@@ -18,6 +18,15 @@ import ReadingRecordForm from './ReadingRecordForm';
 const BookDetail = () => {
   const { isOpen, close, scrollPos, open } = useModal();
   const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [isClamped, setIsClamped] = useState<boolean>(false);
+  const plotRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (plotRef.current) {
+      const plot = plotRef.current;
+      setIsClamped(plot.clientHeight < plot.scrollHeight);
+    }
+  }, []);
 
   const navigate = useNavigate();
   const { detailId = '' } = useParams();
@@ -48,6 +57,7 @@ const BookDetail = () => {
   }
 
   const { yy, mm, dd } = formatDateAndTime(book.pubDate);
+
   return (
     book && (
       <div className="height-without-header flex flex-col">
@@ -68,18 +78,20 @@ const BookDetail = () => {
           <div className="w-full pb-6 text-base font-semibold">
             책정보
             <hr className="mb-2 h-0.5 border-none bg-gray" />
-            <div className="grid grid-cols-2 grid-rows-2 text-[0.8rem] font-normal">
+            <div className="grid grid-cols-2 grid-rows-2 text-[0.815rem] font-normal">
               <p className="truncate pr-2">{`출판 : ${book.publisher}`}</p>
               <p className="truncate pr-2">{`카테고리 : ${formatBookJenre(book.genre)}`}</p>
               <p className="truncate pr-2">{`발행 : ${yy}.${mm}.${dd}`}</p>
               <p className="truncate pr-2">{`ISBN : ${book.isbn}`}</p>
             </div>
           </div>
-          <div className="relative grow text-base font-semibold">
+          <div className="relative w-full grow text-base font-semibold">
             줄거리
-            <span className="absolute right-0 text-xs text-sub">더보기</span>
+            {isClamped && <span className="absolute right-0 text-xs text-sub">더보기</span>}
             <hr className="mb-2 h-0.5 border-none bg-gray" />
-            <div className="max-h-[100px] bg-slate-500 text-[13px] font-normal">{book.plot}</div>
+            <div className="line-clamp-6 w-full overflow-hidden break-words text-[0.815rem]" ref={plotRef}>
+              {book.plot}
+            </div>
           </div>
           <Button handleClick={handleSaveBook}>책 저장하기</Button>
         </section>
