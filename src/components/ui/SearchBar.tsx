a610a00704e942fb0d2a69c5ce0dd117a0dbc581
devkyoung2 <devkyoung2@gmail.com>
2024-12-15 22:04:56 +0900

    [refactor] : 여백값 수정

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -19,7 +19,7 @@ const SearchBar = ({ placeholder, value, setValue, fetchResult }: SearchBarProps
   };
 
   return (
-    <form onSubmit={handleSubmit} className="mx-7 mb-3 flex items-center rounded-xl bg-white px-3">
+    <form onSubmit={handleSubmit} className="mx-7 mb-4 flex items-center rounded-xl bg-white px-3">
       <span className="mr-1">
         <FcSearch style={{ width: '28px', height: '28px' }} />
       </span>
--- src/pages/Search/SearchBookResult.tsx
+++ src/pages/Search/SearchBookResult.tsx
@@ -9,8 +9,8 @@ const SearchBookResult = ({ book }: SearchBookResultProps) => {
   const { yy, mm, dd } = formatDateAndTime(book.pubDate);
 
   return (
-    <div className="flex h-[126px] items-center p-2">
-      <img src={book.cover} className="mr-3 h-full w-[70px] drop-shadow-md" alt={`${book.title}책 커버`} />
+    <div className="flex h-[7.9rem] items-center p-2">
+      <img src={book.cover} className="mr-3 h-full w-[4.7rem] drop-shadow-md" alt={`${book.title}책 커버`} />
       <div className="flex h-full flex-grow flex-col justify-between">
         <span className="line-clamp-1 text-sm font-semibold">{book.title}</span>
         <div className="text-[11px] text-sub">
