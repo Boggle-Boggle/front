cdac4beb156625c5af177272ba2fd537ee992428
devkyoung2 <devkyoung2@gmail.com>
2024-12-15 14:35:12 +0900

    [feat] : 하단 네비바에 검색 탭 추가

--- src/components/ui/BottomNavigator.tsx
+++ src/components/ui/BottomNavigator.tsx
@@ -1,11 +1,13 @@
 import { FaRegUserCircle } from 'react-icons/fa';
+import { FiSearch } from 'react-icons/fi';
 import { LuHome } from 'react-icons/lu';
 import { PiBooksDuotone } from 'react-icons/pi';
 import { NavLink } from 'react-router-dom';
 
+// TODO : 선택된 탭 보더 처리
 const BottomNavigator = () => {
   return (
-    <ul className="h-footer fixed bottom-0 grid w-full max-w-screen-sm grid-cols-3 items-center rounded-t-xl bg-white shadow-navigator">
+    <ul className="fixed bottom-0 grid h-footer w-full max-w-screen-sm grid-cols-4 items-center rounded-t-xl bg-white shadow-navigator">
       <li className="m-auto">
         <NavLink
           to="/"
@@ -14,6 +16,17 @@ const BottomNavigator = () => {
           }
         >
           <LuHome style={{ width: '29px', height: '29px', marginBottom: '6px' }} />홈
+        </NavLink>
+      </li>
+      <li className="m-auto">
+        <NavLink
+          to="/search"
+          className={({ isActive }) =>
+            isActive ? 'flex flex-col items-center text-xs font-bold' : 'flex flex-col items-center text-xs'
+          }
+        >
+          <FiSearch style={{ width: '29px', height: '29px', marginBottom: '6px' }} />
+          검색
         </NavLink>
       </li>
       <li className="m-auto">
