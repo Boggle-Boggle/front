88bcf557764ae20910aa008d02708012294d6e2b
devkyoung2 <devkyoung2@gmail.com>
2024-12-31 20:24:27 +0900

    [feat] : 독서노트 작성/수정 로직 구현

--- src/pages/Note/index.tsx
+++ src/pages/Note/index.tsx
@@ -1,12 +1,16 @@
-import { useRef, useState } from 'react';
-import { CiBookmarkPlus, CiShoppingTag } from 'react-icons/ci';
+import { useQueryClient } from '@tanstack/react-query';
+
+import { useEffect, useRef, useState } from 'react';
 import { IoArrowBackOutline } from 'react-icons/io5';
+import { LuBookmarkPlus, LuTags, LuTrash2 } from 'react-icons/lu';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 import Header from 'components/Header';
 
 import useKeyboardStatus from 'hooks/useKeyboardStatus';
-import { addNote } from 'services/record';
+import { addNote, updateNote } from 'services/record';
+
+import { AddNoteParams } from 'types/record';
 
 import bookmark from 'assets/bookmarkBig.png';
 
@@ -14,35 +18,70 @@ const MAX_TITLE = 30;
 const MAX_CONTENT = 256;
 
 const Note = () => {
+  const [noteId, setNoteID] = useState<number>(null);
+
+  const [readDateId, setReadDateId] = useState<number>(null);
+  const [selectedDate, setSelectedDate] = useState<string>(Date());
   const [title, setTitle] = useState<string>('');
   const [content, setContent] = useState<string>('');
+  const [page, setPage] = useState<number>(null);
+  const [pages, setPages] = useState<AddNoteParams['pages']>(null);
+  const [tags, setTags] = useState<string[]>([]);
+
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const isKeyboardActive = useKeyboardStatus();
 
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const location = useLocation();
 
-  const { recordId } = location.state;
-  console.log(`${recordId} 번 독서노트 작성중`);
+  const { recordId, note } = location.state;
 
-  const handleGoBack = () => {
-    navigate(-1);
-  };
+  useEffect(() => {
+    if (note) {
+      const { noteId, title, content, tags, selectedDate, page, pages } = note;
+
+      setNoteID(noteId);
+
+      if (title) setTitle(title);
+      if (content) setContent(content);
+      if (tags.length > 0) setTags([...tags]);
+      if (page) setPage(page);
+      if (pages) setPages({ ...pages });
+      if (selectedDate) setSelectedDate(selectedDate);
+    }
+  }, [note]);
 
   const handleSave = () => {
-    if (title.length === 0) {
-      alert('제목을 적어주세요');
+    if (!recordId) return;
+
+    if (!title && !content) {
+      alert('제목이나 내용을 작성해주세요');
       return;
     }
 
-    if (content.length === 0) {
-      alert('내용을 적어주세요');
-      return;
-    }
+    if (note && noteId) {
+      const newNote: Partial<AddNoteParams> = {};
 
-    if (recordId) {
-      addNote(recordId, { title, content });
-    }
+      // if (selectedDate !== note.selectedDate) newNote.selectedDate = selectedDate;
+      if (title !== note.title) newNote.title = title;
+      if (content !== note.content) newNote.content = content;
+      if (tags.toString() !== note?.tags.toString()) newNote.tags = tags;
+      // if (page) {
+      //   newNote.page = page;
+      //   newNote.pages = null;
+      // }
+      // if (pages) {
+      //   newNote.page = null;
+      //   newNote.pages = pages;
+      // }
+
+      updateNote(recordId, noteId, newNote);
+    } //
+    else addNote(recordId, { readDateId, selectedDate: null, title, content, page, pages, tags });
+
+    queryClient.invalidateQueries({ queryKey: ['note', recordId] });
+    navigate(`/record/${recordId}`);
   };
 
   const handleChangeTitle = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -69,8 +108,12 @@ const Note = () => {
   return (
     <div className="relative h-screen bg-gray">
       <Header
-        title={<>독서 노트</>}
-        leftBtn={<IoArrowBackOutline style={{ width: '24px', height: '24px' }} onClick={handleGoBack} />}
+        title={
+          <button type="button" aria-label="회독 선택">
+            회독정보없음
+          </button>
+        }
+        leftBtn={<IoArrowBackOutline style={{ width: '24px', height: '24px' }} onClick={() => navigate(-1)} />}
         rightBtn={
           <button className="font-black" onClick={handleSave} type="submit">
             저장
@@ -79,7 +122,7 @@ const Note = () => {
       />
       <section className="height-without-footer border-mains flex flex-col overflow-hidden rounded-tl-3xl border border-main bg-white pb-header">
         <img src={bookmark} className="header absolute right-10 top-header block h-12 w-12" alt="" />
-        <p className="w-full px-5 py-3 font-semibold opacity-50 focus:outline-none">2024년 12월 21일</p>
+        <p className="w-full px-5 py-3 font-semibold opacity-50 focus:outline-none">{selectedDate}</p>
         <textarea
           rows={1}
           className="w-full resize-none overflow-hidden border-b-[1px] border-main px-5 pb-3 font-bold focus:outline-none"
@@ -96,21 +139,22 @@ const Note = () => {
           onChange={(e) => handleChangeContent(e)}
         />
       </section>
-      {/* 키보드 활성화 여부에 따라 top-범위 지정 */}
       <div
-        className={`absolute ${isKeyboardActive ? 'bottom-0' : 'bottom-footer'} flex h-10 w-full items-center justify-between bg-main`}
+        className={`absolute ${isKeyboardActive ? 'bottom-0' : 'bottom-footer'} h-13 flex w-full items-center justify-between bg-main`}
       >
-        <section>
-          {/* TODO : 2차 배포에 포함. 노트추가 부가기능 */}
+        <section className="flex">
           <button className="px-3 py-2" onClick={() => {}} type="button" aria-label="페이지 입력하기">
-            <CiBookmarkPlus style={{ width: '24px', height: '24px', color: '#9B9999' }} />
+            <LuBookmarkPlus style={{ width: '20px', height: '20px', color: '#9B9999' }} />
           </button>
           <button className="px-3 py-2" onClick={() => {}} type="button" aria-label="태그 추가하기">
-            <CiShoppingTag style={{ width: '24px', height: '24px', color: '#9B9999' }} />
+            <LuTags style={{ width: '20px', height: '20px', color: '#9B9999' }} />
           </button>
         </section>
-
-        <span className="pr-3 text-sm">{content?.length ?? 0}자/256자</span>
+        <button className="px-3 py-2" onClick={() => {}} type="button" aria-label="태그 추가하기">
+          <LuTrash2 style={{ width: '20px', height: '20px', color: '#9B9999' }} />
+        </button>
+        {/* TODO : 글자수 처리 로직 추후 구현 */}
+        {/* <span className="pr-3 text-sm">{content?.length ?? 0}자/256자</span> */}
       </div>
     </div>
   );
