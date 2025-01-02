bb645243bc4af56368fed2a84ecfdeff3db98093
devkyoung2 <devkyoung2@gmail.com>
2025-01-02 22:56:40 +0900

    [refactor] : 회독 클릭 범위 수정

--- src/pages/Record/shared/ReadDateItem.tsx
+++ src/pages/Record/shared/ReadDateItem.tsx
@@ -17,21 +17,22 @@ const ReadDateItem = ({ notes, startDate, endDate, idx }: ReadDateItemProps) => 
 
   return (
     <>
-      <div className="flex justify-between border-b-4 border-main px-4 py-[0.875rem]">
+      <button
+        className="flex w-full justify-between border-b-4 border-main px-4 py-[0.875rem]"
+        onClick={handleToggle}
+        type="button"
+        aria-label={isToggled ? '도서 정보 자세히 보기' : '도서 정보 간략히 보기'}
+      >
         <p className="font-bold">
           {idx}회독
           {idx > 0 && <span className="ml-1 font-normal opacity-50">{`${startDate}~${endDate}`}</span>}
         </p>
         {isToggled ? (
-          <button onClick={handleToggle} aria-label="도서 정보 자세히 보기" type="button">
-            <GoChevronUp style={{ width: '20px', height: '20px' }} />
-          </button>
+          <GoChevronUp style={{ width: '20px', height: '20px' }} />
         ) : (
-          <button onClick={handleToggle} aria-label="도서 정보 간략히 보기" type="button">
-            <GoChevronDown style={{ width: '20px', height: '20px' }} />
-          </button>
+          <GoChevronDown style={{ width: '20px', height: '20px' }} />
         )}
-      </div>
+      </button>
       {isToggled && notes.map((note) => <NoteItem note={note} readDateIndex={idx} />)}
     </>
   );
