a9f7578a785cbbcc01f8368ee5dd815e874aee9c
devkyoung2 <devkyoung2@gmail.com>
2025-01-10 23:54:51 +0900

    [feat] : 독서 기록 수정 api 연동

--- src/pages/Edit/index.tsx
+++ src/pages/Edit/index.tsx
@@ -2,13 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 
 import { useEffect, useState } from 'react';
 import { IoArrowBackOutline } from 'react-icons/io5';
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 import Header from 'components/Header';
 
-import { getEditRecord } from 'services/record';
+import { getEditRecord, updateEditRecord } from 'services/record';
 
-import { RecordDate, RecordLibraries, StatusType } from 'types/record';
+import { RecordDate, RecordLibraries, StatusType, UpdateRecordParams } from 'types/record';
 
 import EditLibraries from './EditLibraries';
 import EditRating from './EditRating';
@@ -22,11 +22,30 @@ const Edit = () => {
   const [isVisible, setIsVisible] = useState<boolean>(false);
 
   const location = useLocation();
+  const navigate = useNavigate();
   const { title } = location.state;
 
   const { recordId = '' } = useParams();
 
   const { data } = useQuery({ queryKey: ['edit', recordId], queryFn: () => getEditRecord(recordId) });
+
+  const handleSave = () => {
+    if (!data) return;
+
+    const newRecord: Partial<UpdateRecordParams> = {};
+
+    newRecord.readDateList = readDates;
+
+    const librariesId = libraries.filter((library) => library.selected).map((library) => library.libraryId);
+    newRecord.libraryIdList = librariesId;
+
+    if (rating !== data.rating) newRecord.rating = rating;
+    if (isVisible !== data.isBookVisible) newRecord.isVisible = isVisible;
+
+    updateEditRecord(Number(recordId), newRecord);
+
+    navigate(`/record/${recordId}`);
+  };
 
   useEffect(() => {
     if (!data) return;
@@ -41,8 +60,12 @@ const Edit = () => {
     <div className="bg-white">
       <Header
         title={<p className={`${title.length > 20 ? 'text-sm' : 'text-base'}`}>{title}</p>}
-        leftBtn={<IoArrowBackOutline style={{ width: '24px', height: '24px' }} onClick={() => {}} />}
-        rightBtn={<span className="text-accent">저장</span>}
+        leftBtn={<IoArrowBackOutline style={{ width: '24px', height: '24px' }} onClick={() => navigate(-1)} />}
+        rightBtn={
+          <button className="font-semibold text-accent" type="button" onClick={handleSave}>
+            저장
+          </button>
+        }
       />
 
       {data && (
