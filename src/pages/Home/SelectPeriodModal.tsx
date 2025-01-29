137c36100be1d05f3249b0536b724998621299a6
devkyoung2 <devkyoung2@gmail.com>
2025-01-30 05:53:07 +0900

    [feat] : 메인페이지 헤더 추가

---
+++ src/pages/Home/SelectPeriodModal.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+
+import Button from 'components/Button';
+import HalfScreenModal from 'components/HalfScreenModal';
+import Selector from 'pages/BookDetail/ReadingRecordForm/shared/Selector';
+
+export const DATE_STATUS = {
+  YEARS: [
+    '2001년',
+    '2002년',
+    '2003년',
+    '2004년',
+    '2005년',
+    '2006년',
+    '2007년',
+    '2008년',
+    '2009년',
+    '2010년',
+    '2011년',
+    '2012년',
+    '2013년',
+    '2014년',
+    '2015년',
+    '2016년',
+    '2017년',
+    '2018년',
+    '2019년',
+    '2020년',
+    '2021년',
+    '2022년',
+    '2023년',
+    '2024년',
+    '2025년',
+  ],
+  MONTH: [
+    '01 월',
+    '02 월',
+    '03 월',
+    '04 월',
+    '05 월',
+    '06 월',
+    '07 월',
+    '08 월',
+    '09 월',
+    '10 월',
+    '11 월',
+    '12 월',
+    '전체보기',
+  ],
+};
+
+type SelectPeriodModalProps = {
+  close: () => void;
+};
+
+const SelectPeriodModal = ({ close }: SelectPeriodModalProps) => {
+  const [selectedYear, setSelectedYear] = useState<number | null>(2025);
+  const [selectedMonth, setSelectedMonth] = useState<number | null>(1);
+
+  const handleSelect = () => {
+    close();
+  };
+
+  return (
+    <HalfScreenModal>
+      <div className="absolute bottom-0 left-0 top-0 z-30 flex h-full w-full flex-col rounded-t-2xl bg-white">
+        <p className="py-3 text-center">기간 선택</p>
+        <hr className="h-[2px] border-none bg-accent" />
+        <section className="flex">
+          {selectedYear !== null && selectedMonth !== null && (
+            <>
+              <Selector list={DATE_STATUS.YEARS} selected={selectedYear} setSelected={setSelectedYear} />
+              <Selector list={DATE_STATUS.MONTH} selected={selectedMonth} setSelected={setSelectedMonth} />
+            </>
+          )}
+        </section>
+        <Button className="mx-auto mt-6 w-5/6 font-bold text-white" handleClick={handleSelect}>
+          선택
+        </Button>
+      </div>
+    </HalfScreenModal>
+  );
+};
+
+export default SelectPeriodModal;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,31 @@
-// import Header from 'components/Header';
+import { IoCalendarNumberOutline } from 'react-icons/io5';
+
+import Header from 'components/Header';
+
+import useModal from 'hooks/useModal';
 
 import BookCase from './BookCase';
+import SelectPeriodModal from './SelectPeriodModal';
 
 const Home = () => {
+  const { isOpen, close, open } = useModal();
+
   return (
-    <div className="flex h-full w-full items-center justify-center bg-[#CBBAB9]">
-      {/* <Header /> */}
-      <BookCase />
-    </div>
+    <>
+      <Header
+        backgroundColor="bg-[#CBBAB9]"
+        title="2025년 전체"
+        rightBtn={
+          <button aria-label="기간선택" type="button" onClick={open}>
+            <IoCalendarNumberOutline style={{ width: '24px', height: '24px' }} />
+          </button>
+        }
+      />
+      <div className="flex h-full w-full flex-col items-center justify-center bg-[#CBBAB9]">
+        <BookCase />
+      </div>
+      {isOpen && <SelectPeriodModal close={close} />}
+    </>
   );
 };
 
