b3f176246e119c9e0488e3e4481c0f8f79341315
devkyoung2 <devkyoung2@gmail.com>
2025-02-08 18:39:06 +0900

    [fix] : 목 프로필 이미지 변경

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -10,7 +10,7 @@ import useModal from 'hooks/useModal';
 import { getMyPageInfo, getTermsAgreement } from 'services/user';
 
 import { CommonPencil, MyPageBooks, MyPageMonth, MyPageNotes } from 'assets/icons';
-import ProfileSvg from 'assets/img/profile.svg?react';
+import ProfileSvg from 'assets/img/profile.svg';
 
 import LogoutModal from './LogoutModal';
 import Content from './shared/Content';
@@ -38,9 +38,12 @@ const MyPage = () => {
     terms && (
       <div className={`bg-main px-5 ${isIOS ? 'py-[8.5rem]' : 'py-[6.5rem]'}`}>
         <section className="relative flex h-52 flex-col items-center rounded-lg bg-white pb-4 pt-20">
-          <div className="absolute -top-[4.5rem] h-32 w-32 rounded-[50%] border-[3px] border-white">
-            <ProfileSvg width="100%" height="100%" />
-          </div>
+          <img
+            src={ProfileSvg}
+            className="absolute -top-[4.5rem] h-32 w-32 rounded-[50%] border-[5px] border-white object-cover"
+            alt=""
+          />
+
           <button
             className="flex items-center justify-center text-xl font-bold"
             type="button"
