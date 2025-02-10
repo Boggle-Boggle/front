7c0b7959ac34c1e09d42642a15496c9ebb5a0230
devkyoung2 <devkyoung2@gmail.com>
2025-02-10 16:23:50 +0900

    [fix] : 독서노트가 아무것도 없을 경우 독서기록이 없음을 안내하는 ui 추가

--- src/pages/Record/NoteTab.tsx
+++ src/pages/Record/NoteTab.tsx
@@ -17,19 +17,26 @@ const NoteTab = ({ recordId }: NoteTabProps) => {
     queryFn: () => getNote(recordId!),
   });
 
-  if (isLoading) return <Loading />;
+  if (isLoading || data === undefined) return <Loading />;
 
-  return (
-    data !== undefined &&
-    data.map(({ readDate, notes }, idx) => {
-      const startDate = (readDate.startReadDate && formatDateTimeToDate(readDate.startReadDate)) ?? '';
-      const endDate = readDate.endReadDate ? formatDateTimeToDate(readDate.endReadDate) : '읽는중';
+  const totalNoteCount = data.reduce((pre, cur) => pre + cur.notes.length, 0);
 
-      if (idx === 0 && notes.length === 0) return;
+  if (totalNoteCount === 0)
+    return (
+      <div className="flex h-32 flex-col items-center justify-center bg-white">
+        <p className="font-bold">등록된 독서노트가 없어요</p>
+        <p className="pb-4 pt-2 text-xs opacity-50">아래의 아이콘을 눌러 독서노트를 작성해보세요</p>
+      </div>
+    );
 
-      return <ReadDateItem startDate={startDate} endDate={endDate} notes={notes} idx={idx} />;
-    })
-  );
+  return data.map(({ readDate, notes }, idx) => {
+    const startDate = (readDate.startReadDate && formatDateTimeToDate(readDate.startReadDate)) ?? '';
+    const endDate = readDate.endReadDate ? formatDateTimeToDate(readDate.endReadDate) : '읽는중';
+
+    if (idx === 0 && notes.length === 0) return;
+
+    return <ReadDateItem startDate={startDate} endDate={endDate} notes={notes} idx={idx} />;
+  });
 };
 
 export default NoteTab;
