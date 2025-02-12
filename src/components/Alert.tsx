a3c1edbadcfe8fcdc44ef8a95788a9349853f1a5
devkyoung2 <devkyoung2@gmail.com>
2025-02-12 17:47:08 +0900

    [fix] : position/z-index 값 수정

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -9,9 +9,9 @@ type AlertProps = {
 
 const Alert = ({ message, onClose }: AlertProps) => {
   return (
-    <div className="absolute flex h-full w-full items-center justify-center">
-      <div className="absolute top-0 z-40 h-full w-full bg-black opacity-40" role="presentation" />
-      <section className="z-50 mx-auto w-[75%] rounded-md bg-white px-6 py-6 text-center">
+    <div className="fixed right-0 top-0 z-[60] flex h-full w-full items-center justify-center">
+      <div className="absolute top-0 h-full w-full bg-black opacity-40" role="presentation" />
+      <section className="z-[70] mx-auto w-[75%] rounded-md bg-white px-6 py-6 text-center">
         <img src={alert} alt="" className="m-auto" />
         <p className="mb-7 mt-5 text-lg font-semibold">{message}</p>
         <Button handleClick={onClose}>확인</Button>
