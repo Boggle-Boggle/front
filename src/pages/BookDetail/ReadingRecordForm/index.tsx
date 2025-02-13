83a944323c95faf45fec4e7495b6a4dabdbcb961
devkyoung2 <devkyoung2@gmail.com>
2025-02-13 17:24:08 +0900

    [refactor] : 올바른 z-index 값이 되도록 코드 수정

--- src/pages/BookDetail/ReadingRecordForm/index.tsx
+++ src/pages/BookDetail/ReadingRecordForm/index.tsx
@@ -55,7 +55,7 @@ const ReadingRecordForm = ({ isbn, onClose }: ReadingRecordFormProps) => {
   return (
     <>
       <button
-        className="absolute top-0 z-20 h-full w-full bg-black opacity-40"
+        className="absolute top-0 z-30 h-full w-full bg-black opacity-40"
         onClick={onClose}
         aria-label="취소"
         type="button"
