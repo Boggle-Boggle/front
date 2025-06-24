acb8e54412bd9c8c51e4bded94a3f29cf5c02df4
devkyoung2 <devkyoung2@gmail.com>
2025-06-25 01:40:28 +0900

    [feat] : 새로운 디자인 토큰 정의

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,47 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        // 메인 테마
+        primary: 'var(--color-primary)',
+        'primary-light': 'var(--color-primary-light)',
+        secondary: 'var(--color-secondary)',
+        'secondary-light': 'var(--color-secondary-light)',
+
+        // 시스템 컬러
+        information: '#2c80ff',
+        warning: '#ffd900',
+        success: '#8bcfa7',
+        danger: '#fc6533',
+
+        // 블랙
+        black: '#303030',
+        'neutral-80': '#555555',
+        'neutral-60': '#888888',
+        'neutral-40': '#cacaca',
+        'neutral-20': '#eeeeee',
+        white: '#ffffff',
+      },
+      fontSize: {
+        h1: ['1.75rem', { fontWeight: '700' }],
+        title: ['1.375rem', { fontWeight: '700' }],
+        body: ['1rem', { fontWeight: '500' }],
+        caption: ['0.875rem', { fontWeight: '500' }],
+      },
+      spacing: {
+        mobile: '16px',
+        tablet: '40px',
+      },
+      // 타블렛 반응형
+      // screens: {
+      //   mobile: { max: '600px' },
+      //   tablet: { min: '601px', max: '1200px' },
+      // },
+      /////////////////////////////////////////////////////////////////
+      /////////////////////////////////////////////////////////////////
+      //                           레거시 코드                          //
+      /////////////////////////////////////////////////////////////////
+      /////////////////////////////////////////////////////////////////
+      colors: {
         main: '#EEEDEB',
         accent: '#E6B9A6',
         sub: '#939185',
