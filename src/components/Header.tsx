2880a71d6d28bd089beb342e40dd34dfb22fcd5b
devkyoung2 <devkyoung2@gmail.com>
2025-06-26 16:33:24 +0900

    [remove] : 디버깅용 배경색상 제거

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = ({ leftBtn, title, rightBtn }: HeaderProps) => {
   return (
     <div className="pt-safe-top px-mobile fixed z-50 grid h-12 w-full grid-cols-[48px_auto_48px] items-center">
       <span className={`justify-self-start ${leftBtn || 'invisible'}`}>{leftBtn}</span>
-      <span className="w-full justify-self-center bg-slate-600 px-2 text-center font-semibold">{title}</span>
+      <span className="w-full justify-self-center px-2 text-center font-semibold">{title}</span>
       <span className={`justify-self-end ${rightBtn || 'invisible'}`}>{rightBtn}</span>
     </div>
   );
