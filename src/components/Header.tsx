0bb7b8d80a9347afc859b05b3d86adaaf382b8be
devkyoung2 <devkyoung2@gmail.com>
2025-06-26 16:56:37 +0900

    [feat] : 헤더에 빈 spacer 박스를 추가해서 헤더랑 content를 겹치게 하는 기능 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,19 @@ type HeaderProps = {
   leftBtn?: React.ReactNode;
   title?: React.ReactNode;
   rightBtn?: React.ReactNode;
+  withSpacer?: boolean;
 };
 
-const Header = ({ leftBtn, title, rightBtn }: HeaderProps) => {
+const Header = ({ leftBtn, title, rightBtn, withSpacer = true }: HeaderProps) => {
   return (
-    <div className="pt-safe-top px-mobile fixed z-50 grid h-12 w-full grid-cols-[48px_auto_48px] items-center">
-      <span className={`justify-self-start ${leftBtn || 'invisible'}`}>{leftBtn}</span>
-      <span className="w-full justify-self-center px-2 text-center font-semibold">{title}</span>
-      <span className={`justify-self-end ${rightBtn || 'invisible'}`}>{rightBtn}</span>
-    </div>
+    <>
+      <div className="pt-safe-top px-mobile fixed z-50 grid h-12 w-full grid-cols-[48px_auto_48px] items-center">
+        <span className={`justify-self-start ${leftBtn || 'invisible'}`}>{leftBtn}</span>
+        <span className="w-full justify-self-center px-2 text-center font-semibold">{title}</span>
+        <span className={`justify-self-end ${rightBtn || 'invisible'}`}>{rightBtn}</span>
+      </div>
+      <div className={`pt-safe-top ${withSpacer && 'h-12'}`} />
+    </>
   );
 };
 
