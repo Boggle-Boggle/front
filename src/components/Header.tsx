d35aebe7b779ee1ae8c802bc976ecaa062d42049
devkyoung2 <devkyoung2@gmail.com>
2025-07-02 22:55:00 +0900

    [refactor] : 변경된 폰트토큰에 맞게 헤더 업데이트

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,9 @@ type HeaderProps = {
 const Header = ({ leftBtn, title, rightBtn, withSpacer = true }: HeaderProps) => {
   return (
     <>
-      <div className="pt-safe-top px-mobile fixed z-50 grid h-12 w-full grid-cols-[48px_auto_48px] items-center">
+      <div className="fixed z-50 grid h-12 w-full grid-cols-[48px_auto_48px] items-center px-mobile pt-safe-top">
         <span className={`justify-self-start ${leftBtn || 'invisible'}`}>{leftBtn}</span>
-        <span className="w-full justify-self-center px-2 text-center font-semibold">{title}</span>
+        <span className="text-body1 w-full justify-self-center px-2 text-center">{title}</span>
         <span className={`justify-self-end ${rightBtn || 'invisible'}`}>{rightBtn}</span>
       </div>
       {withSpacer && (
