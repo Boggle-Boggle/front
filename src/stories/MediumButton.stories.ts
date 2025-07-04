f78732fa8a055738e365207430a5e2a557ef75d0
devkyoung2 <devkyoung2@gmail.com>
2025-07-04 13:00:38 +0900

    [feat] : 버튼 스토리 작성

---
+++ src/stories/MediumButton.stories.ts
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import { Button } from 'components/refactor/Button';
+
+const meta = {
+  title: 'Components/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    onClick: { action: 'clicked' },
+    disabled: { control: 'boolean' },
+    type: {
+      control: { type: 'select' },
+      options: ['button', 'submit'],
+    },
+    width: {
+      control: { type: 'select' },
+      options: ['long', 'short'],
+    },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium'],
+    },
+    variant: {
+      control: { type: 'select' },
+      options: ['primary', 'grey', 'primaryLine', 'warning'],
+    },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const buttonArgs: React.ComponentProps<typeof Button> = {
+  onClick: () => {},
+  children: 'Button',
+  variant: 'primary',
+  size: 'small',
+  width: 'short',
+  disabled: false,
+  type: 'submit',
+};
+
+export const dafault: Story = {
+  args: {
+    ...buttonArgs,
+  },
+};
+
+// export const SmallButtons: StoryFn<typeof Button> = (args) => (
+//   <div style={{ display: 'flex', gap: '8px' }}>
+//     <Button {...args} variant="primary" size="small">
+//       Primary Small
+//     </Button>
+//     <Button {...args} variant="grey" size="small">
+//       Grey Small
+//     </Button>
+//     <Button {...args} variant="warning" size="small">
+//       Warning Small
+//     </Button>
+//   </div>
+// );
