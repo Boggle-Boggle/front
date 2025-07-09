8f6a72392a654ea6cb3a860bf2ccb60d18162e05
devkyoung2 <devkyoung2@gmail.com>
2025-07-09 23:02:48 +0900

    [feat] : 토스트 컴포넌트 구현

---
+++ src/components/refactor/Toast.tsx
@@ -0,0 +1,37 @@
+import { IconCircleCancel, IconCircleCheck, IconCircleInfo } from 'components/icons';
+
+type ToastProps = {
+  type: 'info' | 'error' | 'success';
+  description: string;
+  title?: string;
+  dismissible?: boolean;
+};
+
+const Toast = ({ type, description, title, dismissible = false }: ToastProps) => {
+  const borderClass = type === 'info' ? 'border-information' : type === 'error' ? 'border-danger' : 'border-primary';
+  const icon =
+    type === 'info' ? (
+      <IconCircleInfo className="mr-1 text-information" />
+    ) : type === 'error' ? (
+      <IconCircleCancel className="mr-1 text-danger" />
+    ) : (
+      <IconCircleCheck className="mr-1 text-primary" />
+    );
+
+  return (
+    <div className={`${borderClass} rounded-xl border px-4 py-2`}>
+      {title && (
+        <div className="mb-1 flex items-center text-body2 text-neutral-80">
+          {icon}
+          {title}
+        </div>
+      )}
+      <div className={`flex items-center text-caption1 ${title ? 'text-neutral-60' : 'text-neutral-80'}`}>
+        {!title && icon}
+        {description}
+      </div>
+    </div>
+  );
+};
+
+export default Toast;
