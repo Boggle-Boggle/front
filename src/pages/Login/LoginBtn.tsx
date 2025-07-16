d895525ef8a346b2ba892a46dad95f158dae68fc
devkyoung2 <devkyoung2@gmail.com>
2025-07-17 03:25:31 +0900

    [refactor] : 로그인 버튼 리팩토링
    - 로그인 버튼 전체 리팩토링
    - 최근 로그인 컴포넌트 구현

--- src/pages/Login/LoginBtn.tsx
+++ src/pages/Login/LoginBtn.tsx
@@ -2,44 +2,53 @@ import appleLogoImg from 'assets/logo/apple.png';
 import googleLogoImg from 'assets/logo/google.png';
 import kakaoLogoImg from 'assets/logo/kakao.png';
 
+import Bubble from './Bubble';
+
 const baseUrl = import.meta.env.VITE_SERVER_BASE_URL;
 
 const LOGIN_TYPE = {
   kakao: {
     title: '카카오',
     imgUrl: kakaoLogoImg,
-    redirect: `/oauth2/authorization/kakao?redirect_uri=${import.meta.env.VITE_LOGIN_REDIRECT_URL}`,
+    redirectUrl: `/oauth2/authorization/kakao?redirect_uri=${import.meta.env.VITE_LOGIN_REDIRECT_URL}`,
   },
   google: {
     title: '구글',
     imgUrl: googleLogoImg,
-    redirect: `/oauth2/authorization/google?redirect_uri=${import.meta.env.VITE_LOGIN_REDIRECT_URL}`,
+    redirectUrl: `/oauth2/authorization/google?redirect_uri=${import.meta.env.VITE_LOGIN_REDIRECT_URL}`,
   },
   apple: {
     title: '애플',
     imgUrl: appleLogoImg,
-    redirect: `/oauth2/apple?redirect_uri=${import.meta.env.VITE_LOGIN_REDIRECT_URL}`,
+    redirectUrl: `/oauth2/apple?redirect_uri=${import.meta.env.VITE_LOGIN_REDIRECT_URL}`,
   },
 };
 
-type LoginType = typeof LOGIN_TYPE;
+export type LoginType = typeof LOGIN_TYPE;
 
 type LoginBtnProps = {
   type: keyof LoginType;
-  setIsLoading: (isLoading: boolean) => void;
+  isRecent: boolean;
+  // setIsLoading: (isLoading: boolean) => void;
 };
 
-const LoginBtn = ({ type, setIsLoading }: LoginBtnProps) => {
-  const { title, redirect, imgUrl } = LOGIN_TYPE[type];
+const LoginBtn = ({ type, isRecent }: LoginBtnProps) => {
+  const { title, redirectUrl, imgUrl } = LOGIN_TYPE[type];
 
   const handleClick = () => {
-    setIsLoading(true);
-    window.location.href = baseUrl + redirect;
+    // setIsLoading(true);
+    window.location.href = baseUrl + redirectUrl;
   };
 
   return (
-    <button type="button" aria-label={`${title}로그인`} onClick={handleClick}>
-      <img src={imgUrl} alt="" className="h-12 w-12 rounded-[50%] shadow-xl" />
+    <button
+      className="relative flex flex-col items-center justify-center"
+      type="button"
+      aria-label={`${title}로그인`}
+      onClick={handleClick}
+    >
+      <img src={imgUrl} alt="" className="size-[3.375rem] rounded-[50%] shadow-[0px_2px_10px_0px_#00000024]" />
+      {isRecent && <Bubble />}
     </button>
   );
 };
