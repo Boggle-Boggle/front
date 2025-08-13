f96dbbe566a219f6afa5b1a3e2d4256200be0064
devkyoung2 <devkyoung2@gmail.com>
2025-08-14 01:43:44 +0900

    [remove] : 사라진 api 요청 함수를 사용하고 있는 부분의 api 요청 코드를 제거함

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,48 +1,14 @@
-import { useQuery } from '@tanstack/react-query';
-
-import { useState } from 'react';
-
 import Header from 'components/Header';
 
-import useModal from 'hooks/useModal';
-import { getBookCase } from 'services/record';
-
 import BookCase from './BookCase';
-import SelectPeriodModal from './SelectPeriodModal';
 
 const Home = () => {
-  const [title, setTitle] = useState<string>('2025년 전체보기');
-  const [selectedYear, setSelectedYear] = useState<number | null>(25);
-  const [selectedMonth, setSelectedMonth] = useState<number | null>(13);
-
-  const { data: books, refetch } = useQuery({
-    queryKey: ['book', null, null],
-    queryFn: () => getBookCase(selectedYear, selectedMonth),
-  });
-
-  const { isOpen, close, open } = useModal();
-
   return (
     <>
-      <Header
-        withSpacer={false}
-        title={`${title} (${books?.length ?? 0})`}
-        rightBtn={<button aria-label="기간선택" type="button" onClick={open} />}
-      />
+      <Header withSpacer={false} title="타이틀" rightBtn={<button aria-label="기간선택" type="button" />} />
       <div className="flex h-dvh flex-col items-center justify-center overflow-scroll">
         <BookCase />
       </div>
-      {isOpen && (
-        <SelectPeriodModal
-          setTitle={setTitle}
-          selectedYear={selectedYear}
-          setSelectedYear={setSelectedYear}
-          selectedMonth={selectedMonth}
-          setSelectedMonth={setSelectedMonth}
-          close={close}
-          fetchBooks={refetch}
-        />
-      )}
     </>
   );
 };
