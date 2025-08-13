a7c350f59d009b5f12a15882893b62e36b62be13
devkyoung2 <devkyoung2@gmail.com>
2025-08-14 01:43:08 +0900

    [refactor] : 가입 완료 페이지 수정
    - 회원가입이 성공할 경우에만 해당 페이지로 진입할 수 있도록 함

--- src/pages/SignUp/Complete.tsx
+++ src/pages/SignUp/Complete.tsx
@@ -1,37 +1,12 @@
-import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { BottomButton } from 'components/refactor/Button';
 import Highlight from 'components/refactor/Highlight';
 
-import { agreeTerms, refreshToken, updateNickname } from 'services/user';
-
 import signup_complete from 'assets/img/signup_complete.png';
 
-type CompleteProps = {
-  nickName: string;
-};
-
-const Complete = ({ nickName }: CompleteProps) => {
+const Complete = () => {
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const completeSignUp = async () => {
-      try {
-        await updateNickname(nickName);
-        await agreeTerms([
-          { id: 1, isAgree: true },
-          { id: 2, isAgree: true },
-        ]);
-        await refreshToken();
-      } catch (error) {
-        // TODO : 위의 상황에서 에러 발생시 로그인 페이지로 이동(각각확인)
-        navigate('/login');
-      }
-    };
-
-    completeSignUp();
-  }, [nickName, navigate]);
 
   return (
     <div className="h-dvh pb-2 text-center">
