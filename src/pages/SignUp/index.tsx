03c78f1b66ec87c784a8e179137a4a983ac0b056
devkyoung2 <devkyoung2@gmail.com>
2025-08-13 23:50:38 +0900

    [refactor] : SignUp 페이지 로직 수정
    - 해당 페이지에서는 스텝만 관리하도록 수정

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,42 +1,23 @@
-import { useQuery } from '@tanstack/react-query';
-
 import { useState } from 'react';
-
-import useNickNameInput from 'hooks/useNickNameInput';
-import { getTermsAgreement } from 'services/user';
+import { useNavigate } from 'react-router-dom';
 
 import Complete from './Complete';
-import NickNameInput from './NickNameInput';
+import NicknameInput from './NicknameInput';
 import TermsAgreement from './TermsAgreement';
 
 type StepType = '닉네임입력' | '약관동의' | '가입완료';
 
 const SignUp = () => {
   const [step, setStep] = useState<StepType>('닉네임입력');
-
-  const { nickName, saveNickName, changeNickName } = useNickNameInput(() => setStep('약관동의'));
-
-  const { data } = useQuery({
-    queryKey: ['termsAgreement'],
-    queryFn: () => getTermsAgreement(),
-  });
-
-  // const createTermsAgreement = () => {
-  //   return data?.terms.map((term) => ({
-  //     id: term.id,
-  //     isAgree: true,
-  //   }));
-  // };
+  const navigate = useNavigate();
 
   return (
     <>
-      {step === '닉네임입력' && (
-        <NickNameInput nickName={nickName} changeNickName={changeNickName} saveNickName={saveNickName} />
+      {step === '닉네임입력' && <NicknameInput onPrev={() => navigate('/home')} onNext={() => setStep('약관동의')} />}
+      {step === '약관동의' && (
+        <TermsAgreement onPrev={() => setStep('닉네임입력')} onNext={() => setStep('가입완료')} />
       )}
-      {step === '약관동의' && data?.terms && (
-        <TermsAgreement terms={data.terms} onPrev={() => setStep('닉네임입력')} onNext={() => setStep('가입완료')} />
-      )}
-      {step === '가입완료' && <Complete nickName={nickName} />}
+      {step === '가입완료' && <Complete />}
     </>
   );
 };
