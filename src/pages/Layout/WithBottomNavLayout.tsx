0bfe958658c832b3e5b57c9ef0c8347c995dcf60
devkyoung2 <devkyoung2@gmail.com>
2025-08-19 04:01:50 +0900

    [fix] : 애니메이션 네이밍 변경 및 불필요한 스타일 속성 제거

--- src/pages/Layout/WithBottomNavLayout.tsx
+++ src/pages/Layout/WithBottomNavLayout.tsx
@@ -4,7 +4,7 @@ import BottomNavigator from 'components/BottomNavigator';
 
 const WithBottomNavLayout = () => {
   return (
-    <section className="h-dvh w-full overflow-scroll">
+    <section className="h-dvh w-full">
       <Outlet />
       <BottomNavigator />
     </section>
