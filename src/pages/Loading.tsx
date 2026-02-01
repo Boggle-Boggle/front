fa793a751bd6e6ed182c9bd4e78eb28c3aec3599
devkyoung2 <devkyoung2@gmail.com>
2026-02-01 14:14:12 +0900

    레거시 IMG 제거

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,9 +1,7 @@
-import SpinnerImg from 'assets/Spinner.gif';
-
 const Loading = () => {
   return (
     <section className="absolute left-0 top-0 flex h-full w-full items-center justify-center">
-      <img src={SpinnerImg} alt="로딩중" className="w-14" />
+      <img alt="로딩중" className="w-14" />
     </section>
   );
 };
