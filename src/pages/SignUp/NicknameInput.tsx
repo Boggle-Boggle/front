859d2ed8b18dd64de94fc5630470db97422d19aa
devkyoung2 <devkyoung2@gmail.com>
2026-02-01 16:22:24 +0900

    wip

--- src/pages/SignUp/NicknameInput.tsx
+++ src/pages/SignUp/NicknameInput.tsx
@@ -1,11 +1,10 @@
 import useNicknameStore from 'stores/useNicknameStore';
 import useToastStore from 'stores/useToastStore';
 
-import Header from 'components/Header';
-import { IconCircleCancelFilled } from 'components/icons';
 import { BottomButton } from 'components/Button';
 import IconButton from 'components/Button/IconButton';
-import Highlight from 'components/refactor/Highlight';
+import Header from 'components/Header';
+import { IconCircleCancelFilled } from 'components/icons';
 
 import useInput from 'hooks/useInput';
 import validateNickname from 'utils/validateNickname';
@@ -36,12 +35,12 @@ const NicknameInput = ({ onPrev, onNext }: NicknameInputProps) => {
 
   return (
     <>
-      <Header title="회원가입" prev={onPrev} />
+      <Header title="회원가입" />
       <section className="h-full px-mobile">
         <h1 className="mt-10 whitespace-pre-line text-h1">
-          <Highlight>빼곡에서 사용하실</Highlight>
+          {/* <Highlight>빼곡에서 사용하실</Highlight>
           {'\n'}
-          <Highlight>닉네임을 만들어주세요</Highlight>
+          <Highlight>닉네임을 만들어주세요</Highlight> */}
         </h1>
         <p className="mb-6 pt-2 text-body2 text-warning">
           ⚠️ 다른 이용자에게 불쾌감을 줄 수 있는 단어나 욕설, 선정적 표현은 신고가 누적 될 경우 닉네임이 변경 될 수
@@ -58,9 +57,7 @@ const NicknameInput = ({ onPrev, onNext }: NicknameInputProps) => {
             onChange={(e) => setInput(e.target.value)}
           />
 
-          <IconButton onClick={clear} size="small" label="입력된 검색어 삭제">
-            <IconCircleCancelFilled className="size-icon-md" />
-          </IconButton>
+          <IconButton onClick={clear} label="입력된 검색어 삭제" icon={IconCircleCancelFilled} />
         </form>
         <p className="pt-1 text-caption1 text-neutral-40">글자수 한글 15자/영문 45자 제한</p>
 
