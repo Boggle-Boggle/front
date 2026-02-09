b506b3f3d8c167625cdeb7fc8a2160bb4b579bc6
devkyoung2 <devkyoung2@gmail.com>
2026-02-09 23:58:20 +0900

    잘못된 스타일 수정

--- src/pages/Search/shared/HorizontalBookList.tsx
+++ src/pages/Search/shared/HorizontalBookList.tsx
@@ -12,7 +12,7 @@ export const HorizontalBookList = () => {
     <div className="relative w-full overflow-hidden pb-5 pl-mobile">
       <ul className="scrollbar-hide flex w-full gap-[0.625rem] overflow-x-auto">
         {items.map(({ id, title, author, url }) => (
-          <li key={id} className="h-[13.25rem] w-[6.25rem] shrink-0">
+          <li key={id} className="w-[6.25rem] shrink-0">
             <BookCover size="medium" url={url} />
             <p className="text-title3">{title}</p>
             <p className="text-caption1 text-neutral-40">{author}</p>
