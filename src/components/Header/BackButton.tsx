7fc38a2f2235bc664567c060140b0a830fc533c3
devkyoung2 <devkyoung2@gmail.com>
2026-02-23 22:31:58 +0900

    컨벤션에 맞게 로직 수정

--- src/components/Header/BackButton.tsx
+++ src/components/Header/BackButton.tsx
@@ -7,10 +7,8 @@ type BackButtonProps = {
   onClick?: () => void;
 };
 
-const BackButton = ({ onClick }: BackButtonProps) => {
+export const BackButton = ({ onClick }: BackButtonProps) => {
   const navigate = useNavigate();
 
   return <IconButton onClick={onClick || (() => navigate(-1))} label="뒤로가기" icon={IconArrowLeft} />;
 };
-
-export default BackButton;
