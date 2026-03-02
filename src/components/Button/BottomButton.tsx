c992164768199535ce20b49dbf5fa25ca7941216
devkyoung2 <devkyoung2@gmail.com>
2026-03-02 13:02:16 +0900

    바텀 버튼

--- src/components/Button/BottomButton.tsx
+++ src/components/Button/BottomButton.tsx
@@ -2,15 +2,10 @@ import Button from './Button';
 import { ButtonProps } from './type';
 
 const BottomButton = (props: ButtonProps) => {
-  const { size } = props;
-
   return (
-    <>
-      <div className={`mb-safe-bottom ${size === 'small' ? 'h-11' : 'h-14'}`} />
-      <div className="fixed bottom-0 left-1/2 z-fixedBtn w-full max-w-mobile -translate-x-1/2 bg-neutral-0 bg-opacity-0 px-mobile pb-safe-bottom pt-2">
-        <Button {...props} />
-      </div>
-    </>
+    <div className="fixed inset-x-0 bottom-0 z-fixedBtn mx-auto w-full max-w-mobile bg-neutral-0 px-mobile pb-safe-bottom pt-4">
+      <Button {...props} />
+    </div>
   );
 };
 
