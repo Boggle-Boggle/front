22c8f2ce43253626a047aeedc9cd0794b32d3824
devkyoung2 <devkyoung2@gmail.com>
2026-05-20 00:04:50 +0900

    하이라이트 여백 추가

--- src/components/Highlight.tsx
+++ src/components/Highlight.tsx
@@ -5,7 +5,7 @@ type HighlightProps = {
 
 const Highlight = ({ text, className }: HighlightProps) => {
   return (
-    <span className={`relative inline-block ${className ?? ''}`}>
+    <span className={`relative inline-block px-1 ${className ?? ''}`}>
       <span className="relative z-highlight">{text}</span>
       <span className="pointer-events-none absolute bottom-0 left-0 h-3 w-full bg-primary opacity-50" />
       <span className="pointer-events-none absolute bottom-0 right-0 h-3 w-1 bg-primary opacity-50" />
