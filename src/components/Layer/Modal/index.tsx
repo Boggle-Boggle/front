795537af2a4350a88fb01b723e507349d04639ef
devkyoung2 <devkyoung2@gmail.com>
2026-06-06 15:46:39 +0900

    모달 내부 패딩 추가

--- src/components/Layer/Modal/index.tsx
+++ src/components/Layer/Modal/index.tsx
@@ -16,7 +16,7 @@ export const Modal = ({ children }: ModalProps) => {
         <div
           onAnimationEnd={handleAnimationEnd}
           style={{ boxShadow: '0px 2px 20px 0px #00000029' }}
-          className={`absolute left-1/2 top-1/2 z-layer w-[calc(100%-2rem)] max-w-mobile -translate-x-1/2 -translate-y-1/2 rounded-[20px] bg-neutral-0 ${
+          className={`absolute left-1/2 top-1/2 z-layer w-[calc(100%-2rem)] max-w-mobile -translate-x-1/2 -translate-y-1/2 rounded-[20px] bg-neutral-0 p-5 ${
             visible ? 'animate-fadeIn' : 'animate-fadeOut'
           }`}
         >
