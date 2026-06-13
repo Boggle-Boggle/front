8790f6f6510b462baa8156522f9d2c0b88768359
devkyoung2 <devkyoung2@gmail.com>
2026-06-13 15:55:50 +0900

    회원탈퇴 확인 모달 추가

---
+++ src/pages/MyPage/Account/Withdraw/WithdrawConfirmModal.tsx
@@ -0,0 +1,35 @@
+import { Button } from 'components/Button';
+import { Modal } from 'components/Layer/Modal';
+
+type WithdrawConfirmModalProps = {
+  onCancel: () => void;
+  onConfirm: () => void;
+};
+
+const MSG_WITHDRAW_MODAL_TITLE = '회원 탈퇴 완료하기';
+const MSG_WITHDRAW_MODAL_DESCRIPTION =
+  '회원 탈퇴 시 그동안의 이용 기록과 개인 정보가 모두 삭제되며, 복구가 어려우니 불편하시더라도 잠시만 신중히 고민해 주세요.';
+const MSG_WITHDRAW_MODAL_CANCEL = '뒤로가기';
+const MSG_WITHDRAW_MODAL_CONFIRM = '계정을 삭제합니다';
+
+const WithdrawConfirmModal = (props: WithdrawConfirmModalProps) => {
+  const { onCancel, onConfirm } = props;
+
+  return (
+    <Modal>
+      <h2 className="pb-1 text-title2">{MSG_WITHDRAW_MODAL_TITLE}</h2>
+      <p className="whitespace-pre-line text-body1 text-neutral-60">{MSG_WITHDRAW_MODAL_DESCRIPTION}</p>
+
+      <div className="flex items-center gap-1 pt-5">
+        <Button onClick={onCancel} variant="grey" size="small">
+          {MSG_WITHDRAW_MODAL_CANCEL}
+        </Button>
+        <Button onClick={onConfirm} variant="warning" size="small">
+          {MSG_WITHDRAW_MODAL_CONFIRM}
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
+export default WithdrawConfirmModal;
