29c5c0302b07e01cdca99f23802c64c7c73f3477
devkyoung2 <devkyoung2@gmail.com>
2026-06-13 17:24:59 +0900

    섹션 헤더 여백 토큰 정리

--- src/pages/MyPage/shared/SectionHeader.tsx
+++ src/pages/MyPage/shared/SectionHeader.tsx
@@ -5,5 +5,5 @@ type SectionHeaderProps = {
 export const SectionHeader = (props: SectionHeaderProps) => {
   const { title } = props;
 
-  return <div className="bg-neutral-20 px-4 py-3 text-body1">{title}</div>;
+  return <div className="bg-neutral-20 px-mobile py-3 text-body1">{title}</div>;
 };
