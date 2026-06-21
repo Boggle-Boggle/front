bc3c804c43fc8663e518ed2335af44fb32e3dba4
devkyoung2 <devkyoung2@gmail.com>
2026-06-21 11:05:34 +0900

    스트릭모드 임시 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import { StrictMode, Suspense } from 'react';
+import { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { DeviceProvider } from 'stores/useDeviceStore';
 
@@ -18,13 +18,13 @@ const queryClient = new QueryClient({
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <DeviceProvider>
-      <QueryClientProvider client={queryClient}>
-        <Suspense fallback={<Loading fullscreen />}>
-          <AppRouter />
-        </Suspense>
-      </QueryClientProvider>
-    </DeviceProvider>
-  </StrictMode>,
+  // <StrictMode>
+  <DeviceProvider>
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<Loading fullscreen />}>
+        <AppRouter />
+      </Suspense>
+    </QueryClientProvider>
+  </DeviceProvider>,
+  // </StrictMode>,
 );
