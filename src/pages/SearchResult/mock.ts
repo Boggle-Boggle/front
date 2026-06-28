a3559d0c412cec6fa3a48a8a76acf79ab43ab589
devkyoung2 <devkyoung2@gmail.com>
2026-06-28 19:44:31 +0900

    모킹 제거

--- src/pages/SearchResult/mock.ts
+++ src/pages/SearchResult/mock.ts
@@ -1,34 +1,3 @@
-import type { PaginationMockResponse } from 'api.types';
-
-import { Book } from 'types/book';
-
-const MOCK_BOOKS: Book[] = Array.from({ length: 50 }, (_, i) => ({
-  isbn: `mock-isbn-${i + 1}`,
-  title: `Mock Book Title ${i + 1}`,
-  author: `Mock Author ${i + 1}`,
-  publisher: `Mock Publisher ${i + 1}`,
-  pubDate: '2023-01-01',
-  cover: 'https://image.aladin.co.kr/product/38242/41/cover500/k462034622_2.jpg',
-}));
-
-export const getSearchBooksMock = async (query: string, page: number): Promise<PaginationMockResponse<Book[]>> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const itemsPerPage = 10;
-      const start = (page - 1) * itemsPerPage;
-      const end = start + itemsPerPage;
-      const paginatedBooks = MOCK_BOOKS.slice(start, end);
-
-      resolve({
-        pageNum: page,
-        totalResultCnt: MOCK_BOOKS.length,
-        itemsPerPage,
-        items: paginatedBooks,
-      });
-    }, 500);
-  });
-};
-
 export const hasReadingRecordMock = async (isbn: string): Promise<number | null> => {
   return new Promise((resolve) => {
     setTimeout(() => {
