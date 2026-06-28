2a323a8be5ea466573b8a66db164f5ec6b64bc17
devkyoung2 <devkyoung2@gmail.com>
2026-06-28 14:18:07 +0900

    닉네임 API 연결 훅

---
+++ src/pages/SignUp/useNicknameAvailabilityMutation.ts
@@ -0,0 +1,32 @@
+import { useMutation } from '@tanstack/react-query';
+
+import { isApiError } from 'api';
+import { useToastStore } from 'stores/useToastStore';
+
+import { getNicknameAvailability } from './api';
+
+const MSG_SIGNUP_NICKNAME_DUPLICATED = '이미 사용 중인 닉네임입니다.';
+const MSG_SIGNUP_NICKNAME_CHECK_FAILED = '닉네임 확인에 실패했습니다. 다시 시도해주세요.';
+
+export const useNicknameAvailabilityMutation = () => {
+  const { addToast } = useToastStore();
+
+  return useMutation({
+    mutationFn: getNicknameAvailability,
+    onError: (error) => {
+      if (isApiError(error) && error.code === 'USER_NICKNAME_DUPLICATED') {
+        addToast({
+          description: MSG_SIGNUP_NICKNAME_DUPLICATED,
+          type: 'error',
+        });
+
+        return;
+      }
+
+      addToast({
+        description: MSG_SIGNUP_NICKNAME_CHECK_FAILED,
+        type: 'error',
+      });
+    },
+  });
+};
