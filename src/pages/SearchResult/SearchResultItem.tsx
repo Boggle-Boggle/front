824645bbb5e2e57c5b72d04a65d5bba626f991a1
devkyoung2 <devkyoung2@gmail.com>
2026-06-30 00:23:18 +0900

    나의책에 등록된 책입니다 모킹 제거

--- src/pages/SearchResult/SearchResultItem.tsx
+++ src/pages/SearchResult/SearchResultItem.tsx
@@ -3,20 +3,14 @@ import { Link } from 'react-router-dom';
 import { BookCover } from 'components/BookCover';
 
 import type { SearchBook } from './api';
-import { useCheckReadingRecordQuery } from './useCheckReadingRecordQuery';
 
 type SearchResultItemProps = {
   book: SearchBook;
 };
 
-const MSG_SEARCH_RESULT_BADGE = '* 나의 책에 등록되어 있는 책입니다';
-
 export const SearchResultItem = (props: SearchResultItemProps) => {
   const { book } = props;
   const { title, author, publisher, coverUrl, isbn13 } = book;
-
-  const { data: readingRecordId } = useCheckReadingRecordQuery(isbn13);
-  const isRegistered = !!readingRecordId;
 
   return (
     <Link to={`/books/${isbn13}`} className="flex w-full gap-5 py-4">
@@ -26,7 +20,6 @@ export const SearchResultItem = (props: SearchResultItemProps) => {
         <p className="line-clamp-2 pb-1 text-title3">{title}</p>
         <p className="line-clamp-1 text-caption1 text-neutral-60">{author}</p>
         <p className="line-clamp-1 text-caption2 text-neutral-60">{publisher}</p>
-        {isRegistered && <p className="pt-[0.5625rem] text-caption1 text-information">{MSG_SEARCH_RESULT_BADGE}</p>}
       </div>
     </Link>
   );
--- src/pages/SearchResult/mock.ts
+++
@@ -1,8 +0,0 @@
-export const hasReadingRecordMock = async (isbn: string): Promise<number | null> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const isRegistered = parseInt(isbn.split('-')[2], 10) % 2 !== 0;
-      resolve(isRegistered ? 123 : null);
-    }, 200);
-  });
-};
