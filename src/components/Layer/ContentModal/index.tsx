8e020e79b160a60e4411dd7657a519166e11e36d
devkyoung2 <devkyoung2@gmail.com>
2026-07-06 02:18:57 +0900

    컨텐츠 모달 여백 조정

--- src/components/Layer/ContentModal/index.tsx
+++ src/components/Layer/ContentModal/index.tsx
@@ -10,13 +10,15 @@ type ContentModalProps = {
   onClose: () => void;
   footer?: ReactNode;
   children?: ReactNode;
+  className?: string;
 };
 
 export const ContentModal = (props: ContentModalProps) => {
-  const { title, onClose, footer, children } = props;
+  const { title, onClose, footer, children, className } = props;
+  const modalClassName = className ? `flex flex-col ${className}` : 'flex flex-col gap-6 px-4 pb-6 pt-4';
 
   return (
-    <Modal className="flex flex-col gap-6 px-4 py-6">
+    <Modal className={modalClassName}>
       <div className="relative flex min-h-12 items-center justify-center">
         <p className="text-center text-body1">{title}</p>
         <IconButton onClick={onClose} label="닫기" size="md" icon={IconCancel} className="absolute right-0" />
