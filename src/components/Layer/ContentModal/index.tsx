0a8bf80b69d2b811472b96e16ba8ef12b41c4ba9
devkyoung2 <devkyoung2@gmail.com>
2026-08-11 16:54:09 +0900

    잘못된 좌우 너비 제거

--- src/components/Layer/ContentModal/index.tsx
+++ src/components/Layer/ContentModal/index.tsx
@@ -16,7 +16,7 @@ export const ContentModal = (props: ContentModalProps) => {
   const { title, onClose, footer, children } = props;
 
   return (
-    <Modal className="flex flex-col px-4 pb-6 pt-4">
+    <Modal className="flex flex-col pb-6 pt-4">
       <div className="flex items-center justify-center pb-6 pt-4">
         <p className="text-center text-body1">{title}</p>
         <IconButton onClick={onClose} label="닫기" size="md" icon={IconCancel} className="absolute right-2" />
