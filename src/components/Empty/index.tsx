5faa5e9b7fb0267de0faef74892c12a841065ed9
devkyoung2 <devkyoung2@gmail.com>
2026-09-14 00:31:27 +0900

    empty 수정

--- src/components/Empty/index.tsx
+++ src/components/Empty/index.tsx
@@ -10,8 +10,8 @@ interface EmptyProps {
 export const Empty = ({ text = '데이터가 없습니다', icon: Icon = IconCircleInfo }: EmptyProps) => {
   return (
     <div className="flex flex-col items-center justify-center gap-2 rounded-lg border border-solid border-neutral-20 px-6 py-8 text-center text-neutral-40">
-      <Icon className="size-4" />
-      <p className="whitespace-pre-line break-keep text-body1">{text}</p>
+      <Icon className="size-icon-md" />
+      <p className="whitespace-pre-line break-keep text-body2">{text}</p>
     </div>
   );
 };
