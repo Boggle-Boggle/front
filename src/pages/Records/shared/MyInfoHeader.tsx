518c43d6895f193598d4e03840f1c964287c0342
devkyoung2 <devkyoung2@gmail.com>
2026-09-13 17:48:03 +0900

    바텀 border/패딩 제거

--- src/pages/Records/shared/MyInfoHeader.tsx
+++ src/pages/Records/shared/MyInfoHeader.tsx
@@ -9,7 +9,7 @@ const MSG_EDIT_START = '수정하기';
 
 export const MyInfoHeader = ({ isEdit, onToggleEdit }: MyInfoHeaderProps) => {
   return (
-    <div className="flex items-center justify-between border-b pb-4">
+    <div className="flex items-center justify-between">
       <span className="text-caption1 text-neutral-60">{MSG_MY_READING_INFO}</span>
       <button type="button" onClick={onToggleEdit} className="text-body2 text-information outline-none">
         {isEdit ? MSG_EDIT_COMPLETE : MSG_EDIT_START}
